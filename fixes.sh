104fe658919ab6d5aedc7ad536792a8264294da9
johker1 <30807923+johker1@users.noreply.github.com>
2017-09-15 00:42:53 +0100

    Update fixes.sh

--- fixes.sh
+++ fixes.sh
@@ -1,2 +1,2 @@
 sed -i '0,/if (sizeof($message->getFrom()) === 0) {/s/if (sizeof($message->getFrom()) === 0) {/$message->setFrom();\n&/' /usr/local/lib/owncloud/lib/private/Mail/Mailer.php
-sed -ei '/if (sizeof($message->getFrom()) === 0) {/,+3 s/^/#/' /usr/local/lib/owncloud/lib/private/Mail/Mailer.php
+sed -ie '/if (sizeof($message->getFrom()) === 0) {/,+3 s/^/#/' /usr/local/lib/owncloud/lib/private/Mail/Mailer.php
