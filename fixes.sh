25b212bb56dfdb8d1c4d8164c475e635d25152e0
johker1 <30807923+johker1@users.noreply.github.com>
2017-09-15 14:47:34 +0100

    Update fixes.sh

--- fixes.sh
+++ fixes.sh
@@ -1,2 +1,5 @@
 sed -i '0,/if (sizeof($message->getFrom()) === 0) {/s/if (sizeof($message->getFrom()) === 0) {/$message->setFrom();\n&/' /usr/local/lib/owncloud/lib/private/Mail/Mailer.php
 sed -ie '/if (sizeof($message->getFrom()) === 0) {/,+3 s/^/#/' /usr/local/lib/owncloud/lib/private/Mail/Mailer.php
+
+sed -i 's/setFrom(array $addresses) {/setFrom(){/' /usr/local/lib/owncloud/lib/private/Mail/Message.php
+sed -i 's+$addresses = $this->convertAddresses($addresses);+$addresses = \\OC_User::getUser();+' /usr/local/lib/owncloud/lib/private/Mail/Message.php
