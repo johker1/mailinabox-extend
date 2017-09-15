51f74992a6e31ce6837021d2e285c22e59277bbc
johker1 <30807923+johker1@users.noreply.github.com>
2017-09-15 15:01:44 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+source $HOME/mailinabox-extend/miab-extend.sh
+source $HOME/mailinabox-extend/miab.sh
+
+cd $HOME/mailinabox
+setup/start
+
+cd $HOME/mailinabox-extend
+spreedme.sh
+collabora.sh
+fixes.sh
+
